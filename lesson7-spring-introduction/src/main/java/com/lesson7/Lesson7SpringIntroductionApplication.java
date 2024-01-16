5220681b237352400a70a30af3e053b4133fe1f1
Karen <karen@pactum.com>
2024-01-16 22:14:54 +0200

    lesson 7 classwork

---
+++ lesson7-spring-introduction/src/main/java/com/lesson7/Lesson7SpringIntroductionApplication.java
@@ -0,0 +1,11 @@
+package com.lesson7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson7SpringIntroductionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson7SpringIntroductionApplication.class, args);
+    }
+}
